2daebe0bc3bc6e3a671c1b34593fc33bdcb464aa
Valentin Aitken <valentin@nalisbg.com>
2019-11-17 08:56:20 +0200

    Clear unused dependencies

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'doc2text.com'
 
-    resource '*',
+    resource '/process_binary/odt',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:post, :options, :head]
   end
 end
